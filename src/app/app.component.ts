3b4278f6b9fa6ec5314ee1ec65e2a63ef259e1f7
Jan Steiner <jansteiner73@gmail.com>
2025-02-21 08:36:44 +0100

    skill-set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { LandingPageComponent } from "./landing-page/landing-page.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
+import { SkillSetComponent } from "./skill-set/skill-set.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, LandingPageComponent, AboutMeComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, LandingPageComponent, AboutMeComponent, SkillSetComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
