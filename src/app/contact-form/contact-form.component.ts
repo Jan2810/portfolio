851dc95bfcd9d15ed5bc26b674fc5c49b3af8750
Jan Steiner <jansteiner73@gmail.com>
2025-02-26 18:03:10 +0100

    footer

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from "@angular/common";
 import { FormsModule } from '@angular/forms';
-
+import { FooterComponent } from './../shared/footer/footer.component';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, FooterComponent],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
