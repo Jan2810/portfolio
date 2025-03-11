e264ecd504933e4acf1fefcfdded9a68c4df0a8e
Jan Steiner <jansteiner73@gmail.com>
2025-03-11 09:59:13 +0100

    language toggle animation

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  engIsActive: boolean = true;
+  deIsActive: boolean = false;
+  activeColor: string = '#F87A55';
+  inactiveColor: string = '#FFFFFF';
+
+  imgSrc: String = './assets/img/header/toggle_left.png';
+
+  toggleToEnglish() {
+    this.imgSrc = './assets/img/header/toggle_left.png';
+    this.engIsActive = true;
+    this.deIsActive = false;
+  }
+
+  toggleToGerman() {
+    this.imgSrc = './assets/img/header/toggle_right.png';
+    this.deIsActive = true;
+    this.engIsActive = false;
+  }
 }
