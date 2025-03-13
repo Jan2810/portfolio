30704a447f0e9ba6f9e469ab6dcde5e08d22110f
Jan Steiner <jansteiner73@gmail.com>
2025-03-13 20:48:36 +0100

    projects routing

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from "@angular/router";
+import { SingleProject, projects } from '../interfaces/single-project.interface';
 
 
 @Component({
--- src/app/single-project-view/single-project-view.component.ts
+++ src/app/single-project-view/single-project-view.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "../shared/header/header.component";
 import { SingleProject, projects } from '../interfaces/single-project.interface';
-import { RouterModule } from '@angular/router';
+import { RouterModule, ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-single-project-view',
@@ -16,12 +16,22 @@ import { RouterModule } from '@angular/router';
   styleUrl: './single-project-view.component.scss'
 })
 export class SingleProjectViewComponent {
-  
-  projectIndex = 0;
-  project: SingleProject = projects[this.projectIndex];
+
+  projectIndex: number = 0;
+  project!: SingleProject;
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit() {
+    const idParam = this.route.snapshot.params['id'];
+    this.projectIndex = +idParam;
+    this.project = projects[this.projectIndex];
+    console.log('Geladenes Projekt:', this.project);
+  }
 
   nextProject() {
     this.projectIndex = (this.projectIndex + 1) % projects.length;
     this.project = projects[this.projectIndex];
+    this.router.navigate(['/project', this.projectIndex])
   }
 }
