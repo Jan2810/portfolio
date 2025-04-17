e47ad0617fbceea7b228775589bd2a2fb89d3b81
Jan Steiner <jansteiner73@gmail.com>
2025-04-17 11:27:05 +0200

    legal notice and translation adjustments

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -2,14 +2,26 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../shared/header/header.component';
 import { BurgerMenuComponent } from '../burger-menu/burger-menu.component';
+import { LanguageService } from '../services/language.service';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, BurgerMenuComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    BurgerMenuComponent
+  ],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
 export class LegalNoticeComponent {
+  
+  isEnglish: boolean = true;
 
+  constructor(private languageService: LanguageService) {
+    this.languageService.language$.subscribe(lang => {
+      this.isEnglish = lang;
+    });
+  }
 }
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { LanguageService } from '../../services/language.service';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
 
   imgSrc: String = "./assets/img/footer/logo_blue.png"
+
+  isEnglish: boolean = true;
+
+  constructor(private languageService: LanguageService) {
+    this.languageService.language$.subscribe(lang => {
+      this.isEnglish = lang;
+    });
+  }
 
   changeImg() {
     this.imgSrc = './assets/img/footer/logo_yellow.png'
